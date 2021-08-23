f0fc715e87aec8a37c060774a9e590aa7321cfde
Anthony <anthony_tonz@ymail.com>
2021-08-23 14:04:42 +0700

    Ganti kalimat dan form

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,7 +9,7 @@
                     <div class="col-lg-10">
                         <div class="box">
                             <div class="title mb20">Forgot Password</div>
-                            <div class="t-forgot">Don't worry, you can get your new password by filling your existing email address. We will send you new one.</div>
+                            <div class="t-forgot">Don't worry, you can get your new password by filling your existing email address. We will send you new one.</div> <br />
                             <form>
                                 <div class="row justify-content-center">
                                     <div class="col-md-8 col-lg-7">
@@ -17,7 +17,7 @@
                                             <label id="email">Email:</label>
                                             <input class="form-control" id="email" name="email" type="text" required="" />
                                         </div>
-                                        <button class="hvr-button full100" type="button" data-toggle="modal" data-target="#modal-success">Submit</button>
+                                        <button class="hvr-button full100" type="submit">Submit</button>
                                         <div class="text-auth"><a href="{{ URL::to('/login') }}">Back</a></div>
                                     </div>
                                 </div>
--- resources/views/auth/recovery.blade.php
+++ resources/views/auth/recovery.blade.php
@@ -9,17 +9,23 @@
                     <div class="col-lg-10">
                         <div class="box">
                             <div class="title mb20">Recover Password</div>
-                            <div class="t-forgot">Please enter the e-mail address you used to register an account below. An e-mail will be sent to you shortly to reset your password.</div>
+                            <div class="t-forgot">Please enter your new password below.</div><br />
                             <form>
-                                <div class="row justify-content-center">
-                                    <div class="col-md-8 col-lg-7">
+                                <div class="row">
+                                    <div class="col-md-6">
                                         <div class="form-group">
-                                            <label id="email">Email:</label>
-                                            <input class="form-control" id="email" name="email" type="text" required="" />
+                                            <label id="new-pass">New Password:</label>
+                                            <input class="form-control" id="new-pass" name="new-pass" type="pass" required="" />
                                         </div>
-                                        <button class="hvr-button full100" type="button" data-toggle="modal" data-target="#modal-success">Submit</button>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label id="confirm-pass">Confirm Password:</label>
+                                            <input class="form-control" id="confirm-pass" name="confirm-pass" type="pass" required="" />
+                                        </div>
                                     </div>
                                 </div>
+                                <button class="hvr-button full100" type="submit">Submit</button>
                             </form>
                         </div>
                     </div>
