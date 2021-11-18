1e086fcba608b3d99e52575481a4a2919c450f81
Anthony <anthony_tonz@ymail.com>
2021-11-18 14:52:26 +0700

    Garis, stamp, sign dijadiin 1 gambar. Kiri dan kanan juga.

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -213,15 +213,14 @@
         </tbody>
     </table>
 
-    <table style="border-collapse: collapse; width: 100%; margin-top: 110px; font-size: 7pt; line-height: 14px;">
+    <table style="border-collapse: collapse; width: 100%; margin-top: 50px; font-size: 7pt; line-height: 14px;">
         <tbody>
             <tr>
                 <td width="50%" style="vertical-align:bottom; text-align: center; padding: 0 50px;">
-                    <div style="border-top: 1px solid #000; padding-top: 5px; color: #000;">Customer Signature</div>
+                    <div><img style="width: 80%;" src="{{ asset('images/img-left.png') }}"/></div>
                 </td>
                 <td width="50%" style="vertical-align:bottom; text-align: center; padding: 0 50px;">
-                    <div><img style="width: 40%; margin: 0 auto 10px;" src="{{ asset('images/signature.png') }}"/></div>
-                    <div style="border-top: 1px solid #000; padding-top: 5px; color: #000;">GT Export Pte Ltd</div>
+                    <div><img style="width: 80%;" src="{{ asset('images/img-right.png') }}"/></div>
                 </td>
             </tr>
         </tbody>
